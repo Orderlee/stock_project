40cd8ba5c26f6fa7669a9c0885b2d4b9919fade5
orderlee <nuncateolvidar3@gmail.com>
2020-10-21 18:47:07 +0900

    10

--- com_stock_api/ext/db.py
+++ com_stock_api/ext/db.py
@@ -4,11 +4,18 @@ Base = declarative_base()
 
 db = SQLAlchemy()
 config = {
-    'user':'stockpsychic',
-    'password':'stockpsychic',
-    'host':'stockpsychic.c2hh6gib8xaa.ap-northeast-2.rds.amazonaws.com',
-    'port':'3306',
-    'database':'stockpsychic'
+    'user' : 'root',
+    'password' : 'root',
+    'host': '127.0.0.1',
+    'port' : '3306',
+    'database' : 'stockdb'
+
+    # 'user':'stockpsychic',
+    # 'password':'stockpsychic',
+    # 'host':'stockpsychic.c2hh6gib8xaa.ap-northeast-2.rds.amazonaws.com',
+    # 'port':'3306',
+    # 'database':'stockpsychic'
+    
 }
 charset ={'utf8':'utf8'}
 url = f"mysql+mysqlconnector://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{config['database']}?charset=utf8"
