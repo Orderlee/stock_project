f8bae5931896971cb9c398cea20cd3aaa6b4b235
orderlee <nuncateolvidar3@gmail.com>
2020-10-29 16:50:49 +0900

    17:00

--- com_stock_api/resources/korea_finance.py
+++ com_stock_api/resources/korea_finance.py
@@ -12,115 +12,12 @@ import pandas as pd
 import json
 
 
-class StockService:
 
-    x_train: object = None
-    y_train: object = None
-    x_validation: object = None
-    y_validation: object = None
-    x_test: object = None
-    y_test: object = None
-    model: object = None
-
-    def __init__(self):
-        self.reader = FileReader()
-
-    def hook(self):
-        self.get_data()
-        self.create_model()
-        self.train_model()
-        self.eval_model()
-        # self.debug_model()
-        # self.get_prob()
-
-    def create_train(self, this):
-        pass
-
-    def create_label(self, this):
-        pass
-
-    def get_data(self):
-        self.reader.context = os.path.join(basedir, 'data/')
-        self.reader.fname = 'lgchem.csv'
-        data = self.reader.csv_to_dframe()
-        #print(data)
-        #(890,7)
-        #data = data.to_numpy()
-        # date,close,open,high,low,volume,stock
-        #"date",
-        xdata = data[["open","high","low","volume"]]
-        #print(xdata)
-        ydata = pd.DataFrame(data["close"])
-        #print(ydata)
-
-        xdata_ss = StandardScaler().fit_transform(xdata)
-        #print(xdata_ss)
-        ydata_ss = StandardScaler(). fit_transform(ydata)
-        #print(ydata_ss)
-
-        
-
-        x_train, x_test, y_train, y_test = train_test_split(xdata_ss,ydata_ss, test_size=0.4)
-        x_test, x_validation, y_test, y_validation = train_test_split(x_test,y_test, test_size=0.4)
-
-        self.x_train = x_train; self.x_test = x_test; self.x_validation = x_validation
-        self.y_train = y_train;  self.y_test = y_test; self.y_validation = y_validation
-        
-        print(self.x_train.shape)
-        print(self.y_test.shape)
-
-    def create_model(self):
-        print('create model')
-        model = keras.Sequential()
-
-        #LSTM으로 바꿔야함
-        model.add(layers.Dense(units=1024, input_dim=4, activation='relu'))
-        model.add(layers.Dense(units=512, activation='relu'))
-        model.add(layers.Dense(units=256, activation='relu'))
-        model.add(layers.Dense(units=128, activation='relu'))
-        model.add(layers.Dense(units=64, activation='relu'))
-        model.add(layers.Dense(units=32, activation='relu'))
-        model.add(layers.Dense(units=1))
-
-        model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-
-        self.model = model
-    
-    def train_model(self):
-        es = EarlyStopping(patience=10)
-
-        # seed = 123
-        # np.random.seed(seed)
-        # tf.set_random_seed(seed)
-        hist = self.model.fit(self.x_train, self.y_train,
-        validation_data=(self.x_validation, self.y_validation), epochs=50, batch_size=16, callbacks=[es])
-        print("loss:"+ str(hist.history['loss']))
-        print("MAE:"+ str(hist.history['mae']))
-
-
-    def eval_model(self):
-        res = self.model.evaluate(self.x_test, self.y_test, batch_size=32)
-        print('loss',res[0],'mae',res[1])
-        
-        xhat = self.x_test
-        yhat = self.model.predict(xhat)
-
-        plt.figure()
-        plt.plot(yhat, label = "predicted")
-        plt.plot(self.y_test,label = "actual")
-        #xlabel=['date']
-
-        plt.legend(prop={'size': 20})
-        plt.show()
-
-        print("Evaluate : {}".format(np.average((yhat - self.y_test)**2)))
-
- 
-        
-        
-if __name__ =='__main__':
-    training = StockService()
-    training.hook()
+# ==============================================================
+# =======================                =======================
+# =======================    Modeling    =======================
+# =======================                =======================
+# ==============================================================
 
 
 class StockDto(db.Model):
@@ -248,8 +145,9 @@ class StockDao(StockDto):
 
 if __name__ == "__main__":
     #StockDao.bulk()
-    s = StockDao()
-    s.bulk()
+    # s = StockDao()
+    # s.bulk()
+    ...
 
     
 
@@ -322,7 +220,7 @@ class Stocks(Resource):
     @staticmethod
     def get():
         sd = StockDao()
-        sd.insert('naver_finance')
+        sd.insert('korea_finance')
     
     @staticmethod
     def get():
@@ -352,3 +250,230 @@ class Access(Resource):
         print(stock.date)
         data = StockDao.login(stock)
         return data[0], 200
+
+
+# ==============================================================
+# ==============================================================
+# ==============================================================
+# ==============================================================
+# ==============================================================
+
+
+
+import matplotlib.pyplot as plt
+import plotly
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+
+class Graph():
+
+    def __init__(self):
+        self.data = os.path.abspath(__file__+"/.."+"/data/")
+        
+    def closeprice(self):
+        
+        path = self.data
+        companys = ['lgchem']#['lgchem','lginnotek']
+        for com in companys:
+            print(f'company:{com}')
+            file_name = com +'.csv'
+            input_file = os.path.join(path,file_name)
+            df = pd.read_csv(input_file ,encoding='utf-8',dtype=str)
+            df = pd.DataFrame(df)
+        #print(df.columns)
+
+        from bokeh.io import show, output_file
+        from bokeh.plotting import figure, show, output_notebook
+        from bokeh.layouts import gridplot
+        from bokeh.models.formatters import NumeralTickFormatter
+
+        output_notebook()
+        inc = df.close >= df.open
+        dec = df.open > df.close
+
+        # 캔들 만들기
+        p_candlechart = figure(plot_width=1050, plot_height=300, x_range=(-1, len(df)), tools=['xpan, crosshair, xwheel_zoom, reset, hover, box_select, save'])
+        p_candlechart.segment(df.index[inc], df.high[inc], df.index[inc], df.low[inc], color="green") #서양은 양봉을 초록 음봉을 빨강
+        p_candlechart.segment(df.index[dec], df.high[dec], df.index[dec], df.low[dec], color="red") #한국은 양봉을 빨간 음봉을 파랑
+        p_candlechart.vbar(df.index[inc], 0.5, df.open[inc], df.close[inc], fill_color="green", line_color="green")
+        p_candlechart.vbar(df.index[dec], 0.5, df.open[dec], df.close[dec], fill_color="red", line_color="red")
+
+        p_volumechart = figure(plot_width=1050, plot_height=200, x_range=p_candlechart.x_range, tools="crosshair")
+        p_volumechart.vbar(df.index, 0.5, df.volume, fill_color="white", line_color="white")
+        p_volumechart.background_fill_color = '#121212'
+
+        # 날짜 표시를 위한 작업
+        major_label = {i: date.strftime('%Y-%m-%d') for i, date in enumerate(pd.to_datetime(df["date"]))}
+        major_label.update({len(df): ''})
+        p_volumechart.xaxis.major_label_overrides = major_label
+        p_volumechart.yaxis[0].formatter = NumeralTickFormatter(format='0,0')
+
+
+        # 그리기
+        p = gridplot([[p_candlechart],[p_volumechart]], toolbar_location=None)
+        show(p)
+
+
+        
+        
+
+        
+        # 반응형 차트 그리는 코드
+        #fig = px.line(df, x='date', y='close', title='{}의 종가(close) Time Series'.format(com))
+        # fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.1,row_heights = [0.7, 0.3],)
+        
+        # fig.add_trace(go.Line(x=df["date"], y=df["close"], name="거래금액"), row=1, col=1)
+        # fig.add_trace(go.Bar(x=df["date"], y=df["volume"], name="거래량"), row=2, col=1)
+        # fig.update_layout(height=300*2, width=350*2,title_text="{}의 종가(close)".format(com))
+
+        # fig.update_xaxes(
+            
+        #     rangeselector=dict(
+        #         buttons=list([
+        #             dict(count=1, label="1d", step="day", stepmode="backward"),
+        #             dict(count=3, label="3d", step="day", stepmode="backward"),
+        #             dict(count=6, label="6d", step="day", stepmode="backward"),
+        #             dict(count=1, label="1m", step="month", stepmode="backward"),
+        #             dict(count=3, label="3m", step="month", stepmode="backward"),
+        #             dict(count=6, label="6m", step="month", stepmode="backward"),
+        #             dict(step="all")
+        #         ])
+        #     )
+        # )
+        # fig.show()
+
+
+
+
+        # plt.figure(figsize=(10,4))
+        # plt.plot(df['date'], df['close'])
+        # plt.xlabel('')
+        # plt.ylabel('close')
+        # plt.tick_params(
+        #     axis='x',          # changes apply to the x-axis
+        #     which='both',      # both major and minor ticks are affected
+        #     bottom=False,      # ticks along the bottom edge are off
+        #     top=False,         # ticks along the top edge are off
+        #     labelbottom=False) # labels along the bottom edge are off
+        # #plt.savefig(company + ".png")
+        # plt.show()
+
+if __name__ == "__main__":
+    g=Graph()
+    g.closeprice()
+
+
+
+
+
+
+# class StockService:
+
+#     x_train: object = None
+#     y_train: object = None
+#     x_validation: object = None
+#     y_validation: object = None
+#     x_test: object = None
+#     y_test: object = None
+#     model: object = None
+
+#     def __init__(self):
+#         self.reader = FileReader()
+
+#     def hook(self):
+#         self.get_data()
+#         self.create_model()
+#         self.train_model()
+#         self.eval_model()
+#         # self.debug_model()
+#         # self.get_prob()
+
+#     def create_train(self, this):
+#         pass
+
+#     def create_label(self, this):
+#         pass
+
+#     def get_data(self):
+#         self.reader.context = os.path.join(basedir, 'data/')
+#         self.reader.fname = 'lgchem.csv'
+#         data = self.reader.csv_to_dframe()
+#         #print(data)
+#         #(890,7)
+#         #data = data.to_numpy()
+#         # date,close,open,high,low,volume,stock
+#         #"date",
+#         xdata = data[["open","high","low","volume"]]
+#         #print(xdata)
+#         ydata = pd.DataFrame(data["close"])
+#         #print(ydata)
+
+#         xdata_ss = StandardScaler().fit_transform(xdata)
+#         #print(xdata_ss)
+#         ydata_ss = StandardScaler(). fit_transform(ydata)
+#         #print(ydata_ss)
+
+        
+
+#         x_train, x_test, y_train, y_test = train_test_split(xdata_ss,ydata_ss, test_size=0.4)
+#         x_test, x_validation, y_test, y_validation = train_test_split(x_test,y_test, test_size=0.4)
+
+#         self.x_train = x_train; self.x_test = x_test; self.x_validation = x_validation
+#         self.y_train = y_train;  self.y_test = y_test; self.y_validation = y_validation
+        
+#         print(self.x_train.shape)
+#         print(self.y_test.shape)
+
+#     def create_model(self):
+#         print('create model')
+#         model = keras.Sequential()
+
+#         #LSTM으로 바꿔야함
+#         model.add(layers.Dense(units=1024, input_dim=4, activation='relu'))
+#         model.add(layers.Dense(units=512, activation='relu'))
+#         model.add(layers.Dense(units=256, activation='relu'))
+#         model.add(layers.Dense(units=128, activation='relu'))
+#         model.add(layers.Dense(units=64, activation='relu'))
+#         model.add(layers.Dense(units=32, activation='relu'))
+#         model.add(layers.Dense(units=1))
+
+#         model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+
+#         self.model = model
+    
+#     def train_model(self):
+#         es = EarlyStopping(patience=10)
+
+#         # seed = 123
+#         # np.random.seed(seed)
+#         # tf.set_random_seed(seed)
+#         hist = self.model.fit(self.x_train, self.y_train,
+#         validation_data=(self.x_validation, self.y_validation), epochs=50, batch_size=16, callbacks=[es])
+#         print("loss:"+ str(hist.history['loss']))
+#         print("MAE:"+ str(hist.history['mae']))
+
+
+#     def eval_model(self):
+#         res = self.model.evaluate(self.x_test, self.y_test, batch_size=32)
+#         print('loss',res[0],'mae',res[1])
+        
+#         xhat = self.x_test
+#         yhat = self.model.predict(xhat)
+
+#         plt.figure()
+#         plt.plot(yhat, label = "predicted")
+#         plt.plot(self.y_test,label = "actual")
+#         #xlabel=['date']
+
+#         plt.legend(prop={'size': 20})
+#         plt.show()
+
+#         print("Evaluate : {}".format(np.average((yhat - self.y_test)**2)))
+
+ 
+        
+        
+# if __name__ =='__main__':
+#     training = StockService()
+#     training.hook()
--- edit.py
+++ edit.py
@@ -1,39 +1,163 @@
-import pandas as pd
 
-class Covidedit():
+# import matplotlib.pyplot as plt
+# %matplotlib inline
+# df_stock['close'].plot(figsize=(12,6), grid=True)
 
-    df_kor = pd.read_csv('/Users/YoungWoo/stock_psychic_api/com_stock_api/resources/data/kr_daily.csv')
-    df_reg = pd.read_csv('/Users/YoungWoo/stock_psychic_api/com_stock_api/resources/data/kr_regional_daily.csv')
-    del df_kor['released']
-    del df_kor['tested']
-    del df_kor['negative']
-    df_kor.columns =['date','total_cases','total_death']
-    df_kor['date']=pd.to_datetime(df_kor['date'].astype(str), format='%Y/%m/%d')
-    print(df_kor)
+# # 반응형 차트 그리는 코드
+# fig = px.line(df, x='date', y='close', title='{}의 종가(close) Time Series'.format(company))
 
-    #print(df_reg)
-    df_reg = df_reg[df_reg['region']=='서울']
-    #print(df_reg)
-    del df_reg['region']
-    del df_reg['released']
-    df_reg.columns =['date','seoul_cases','seoul_death']
-    #print(df_reg)
-    df_reg['date']=pd.to_datetime(df_reg['date'].astype(str), format='%Y/%m/%d')
-    print(df_reg)
+# fig.update_xaxes(
+#     rangeslider_visible=True,
+#     rangeselector=dict(
+#         buttons=list([
+#             dict(count=1, label="1m", step="month", stepmode="backward"),
+#             dict(count=3, label="3m", step="month", stepmode="backward"),
+#             dict(count=6, label="6m", step="month", stepmode="backward"),
+#             dict(step="all")
+#         ])
+#     )
+# )
+# fig.show()
+
+
+# #단순형 차트
+# import matplotlib.pyplot as plt
+# # 필요한 모듈 import 하기 
+# import plotly
+# import plotly.graph_objects as go
+# import plotly.express as px
+
+# # %matplotlib inline 은 jupyter notebook 사용자용 - jupyter notebook 내에 그래프가 그려지게 한다.
+# %matplotlib inline 
+
+# plt.figure(figsize=(10,4))
+# plt.plot(df['date'], df['close'])
+# plt.xlabel('')
+# plt.ylabel('close')
+# plt.tick_params(
+#     axis='x',          # changes apply to the x-axis
+#     which='both',      # both major and minor ticks are affected
+#     bottom=False,      # ticks along the bottom edge are off
+#     top=False,         # ticks along the top edge are off
+#     labelbottom=False) # labels along the bottom edge are off
+# plt.savefig(company + ".png")
+# plt.show()
+
+
+
+
+
+
+# # -*- coding: utf-8 -*- 
+# import os
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# import numpy as np
+# import pandas as pd
+# from pandas import read_table
+# from com_stock_api.utils.file_helper import FileReader
+# from com_stock_api.utils.checker import is_number
+# from collections import defaultdict
+# import math
+# from com_stock_api.naver_news.service_backup import NewsService
+
+# class NewsAnalysis:
+#     def __init__(self,k = 0.5):
+#         self.k = k
+#         self.data = os.path.abspath(__file__+"/.."+"/data/")
+#         #print(self.data) -> /Users/YoungWoo/stock_psychic_api/com_stock_api/naver_news/data
+#         self.reader =FileReader()
+
+
+#     def train(self):
+#         training_set = self.load_corpus()
+#         # 범주 0 (긍정), 범주 1 (부정) 문서의 수를 세어줌
+#         num_class0 = len([1 for _, point in training_set if point > 3.5])
+#         num_class1 = len(training_set) - num_class0
+#         word_counts = self.count_words(training_set)
+#         self.word_probs = self.word_probabilities(word_counts, num_class0, num_class1, self.k)
+
     
-    df_all = pd.merge(df_kor,df_reg, on=['date','date'],how='left')
-    df_all = df_all.fillna(0)
-    df_all['seoul_cases'] = df_all['seoul_cases'].astype(int)
-    df_all['seoul_death'] = df_all['seoul_death'].astype(int)
-    print(df_all)
-    df_all.to_csv('/Users/YoungWoo/stock_psychic_api/com_stock_api/resources/data/kor&seoul.csv',encoding='UTF-8')
-    # df_all = pd.merge(df_kor, df_reg, on=['date','date'], how='left')
 
-    # df_all['total_cases'] = pd.to_numeric(df_all['total_cases'], errors='coerce').fillna(0).astype(int)
-    # df_all['total_deaths'] = pd.to_numeric(df_all['total_deaths'], errors='coerce').fillna(0).astype(int)
-    # df_all['ca_cases'] = pd.to_numeric(df_all['ca_cases'], errors='coerce').fillna(0).astype(int)
-    # df_all['ca_deaths'] = pd.to_numeric(df_all['ca_deaths'], errors='coerce').fillna(0).astype(int)
+#     def load_corpus(self):
+#         reader =self.reader
+#         path = self.data
+#         corpus = read_table(path + '/movie_review.csv', sep=',',encoding='UTF-8')
+#         #print(f'Corpus Spec : {corpus}')
+#         return np.array(corpus)
+    
+#     def count_words(self, traing_set):
+#         counts = defaultdict(lambda: [0,0])
+#         for doc, point in traing_set:
+#             # 영화리뷰가 test일때만 카운팅
+#             if is_number(doc) is False:
+#                 words = doc.split()
+#                 for word in words:
+#                     counts[word][0 if point > 3.5 else 1] += 1
+#         return counts
+    
+#     def word_probabilities(self, counts, total_class0, total_class1, k):
+#         # 단어의 빈도수를 [단어,p(w|긍정), p(w|부정)] 형태로 전환
+#         return [(W,
+#         (class0 + k) / (total_class0 + 2 * k), 
+#         (class1 + k) / (total_class1 + 2 * k))
+#         for W, (class0, class1) in counts.items()]
 
-    # df_all.to_csv(path+"/covid.csv")
-    # print(df_all.head())
+#     def class0_probability(self,word_probs,doc):
+#         # 별도의 토크나이즈 하지 않고 띄어쓰기 
+#         docwords = doc.split()
+#         log_prob_if_class0 = log_prob_if_class1 = 0.0
+#         #모든단어에 반복
+#         for word, prob_if_class0, prob_if_class1 in word_probs:
+#             # 만약 리뷰에 word가 나타나면 해당 단어가 나올 log에 확률을 더해줌
+#             if word in docwords:
+#                 log_prob_if_class0 += math.log(prob_if_class0)
+#                 log_prob_if_class1 += math.log(prob_if_class1)
+#                 # 만약 리뷰에 word 가 없으면 해당 단어가 안나올 log에 확률을 더해줌
+#                 # 나오지 않을 확률은 log ( 1 - 나올 확률) 로 계산
+#             else:
+#                 log_prob_if_class0 += math.log(1.0 - prob_if_class0)
+#                 log_prob_if_class1 += math.log(1.0 - prob_if_class1)
 
+#             prob_if_class0 = math.exp(log_prob_if_class0)
+#             prob_if_class1 = math.exp(log_prob_if_class1)
+
+#             return prob_if_class0 / (prob_if_class0 + prob_if_class1)
+
+  
+
+#     def classify(self,doc):
+#         return self.class0_probability(self.word_probs, doc)
+
+        
+
+#     def hook(self,txt):
+#         print('====hook====')
+#         self.train()
+#         return self.classify(txt)
+        
+
+#     def makelabel(self):
+#         path = self.data
+#         # 1. 수집 
+#         # 2. 모델
+#         #service = NewsService()
+#         #service.new_model()
+#         # 3. CRUD
+#         df_result = pd.read_csv('/Users/YoungWoo/stock_psychic_api/com_stock_api/resources/data/011070.csv',encoding='utf-8', dtype=str)
+#         # 4. Eval
+        
+
+#         df_result['label']= 0.0
+#         for i in range(0,2500):
+#             try:
+#                 df_result['label'][i] = '%.2f' % self.hook(df_result['content'][i])
+#             except KeyError : 
+#                 pass
+        
+#         df_result.to_csv('/Users/YoungWoo/stock_psychic_api/com_stock_api/naver_news/data/lginnotek1.csv',encoding='UTF-8')
+
+#         return df_result
+
+# if __name__=='__main__':
+#     service = NewsAnalysis()
+#     service.makelabel()
